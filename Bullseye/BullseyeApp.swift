7b5af650a536bfdc5fc3ca0f77cd7e135e4a938c
Mozart Sousa <mjasousa@sga.pucminas.br>
2021-12-07 11:35:54 -0300

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Studio Sol on 07/12/21.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
